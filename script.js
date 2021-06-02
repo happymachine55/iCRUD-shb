ef2adbf05f50464b3f232fff9a3550618fc6fb73
happymachine55 <hanbyeol55@naver.com>
2021-06-02 19:07:26 +0900

    crud 사이트 코드 추가

--- script.js
+++ script.js
@@ -11,11 +11,53 @@ var app = new function(){
                 data += '<tr>';
                 data += '<td>' + (i+1) + '. ' + this.tasks[i]+'</td>';
                 data += '<td><button onclick="app.Edit(' + i + ')" class=edit-button>수정</button></td>';
-                data += '<td><button onclick="app.Delete(' + i + ')" class=delete-button>수정</button></td>';
+                data += '<td><button onclick="app.Delete(' + i + ')" class=delete-button>삭제</button></td>';
                 data += '<tr>';
                 data += '</tr>'
             }
         }
         return this.el.innerHTML = data
     };
+
+
+    this.Add = function(){
+        el = document.getElementById('add-todo');
+        var task = el.value;
+        if(task){
+            this.tasks.push(task.trim());
+            this.el.value='';
+            this.FetchAll();
+        }
+    };
+
+
+    this.Edit = function(item){
+        this.el = document.getElementById('edit-todo');
+        el.value = this.tasks[item]
+        document.getElementById('edit-box').style.display='block';
+        self=this;
+
+        document.getElementById('save-edit').onsubmit = function(){
+            var task = el.value;
+            if(task){
+                self.tasks.splice(item, 1, task.trim());
+                self.FetchAll();
+                CloseInput();
+            }
+        }
+    };
+
+
+    this.Delete = function(item){
+        this.tasks.splice(item,1);
+        this.FetchAll();
+    };
+
+
+}
+
+app.FetchAll();
+
+function CloseInput(){
+    document.getElementById('edit-box').style.display='none';
 }
